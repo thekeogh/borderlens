4ddbfad45f570b04af8d3f06db291d0d5d60eecf
Steve McKeogh <steve@screencloud.io>
2026-04-30 11:57:39 +0100

    chore(footer): added github link

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { FaGithub } from "react-icons/fa";
+
 import Style from "./style.module.css";
 
 /**
@@ -9,8 +11,15 @@ export function Footer() {
   return (
     <footer className={Style.root}>
       <div className="container">
-        <p><span>&copy;</span> {new Date().getFullYear()} Borderlens // Not affiliated with Gearbox or 2K</p>
-        <p>Artwork <span>&copy;</span> Borderlens. No reuse without permission. Borderlands and related IP belong to Gearbox Software and 2K.</p>
+        <div className={Style.left}>
+          <p><span>&copy;</span> {new Date().getFullYear()} Borderlens // Not affiliated with Gearbox or 2K</p>
+          <p>Artwork <span>&copy;</span> Borderlens. No reuse without permission. Borderlands and related IP belong to Gearbox Software and 2K.</p>
+        </div>
+        <div className={Style.right}>
+          <a href="https://github.com/thekeogh/borderlens" target="_blank" title="Go to GitHub repo">
+            <FaGithub color="#71717a" size={30} />
+          </a>
+        </div>
       </div>
     </footer>
   );
